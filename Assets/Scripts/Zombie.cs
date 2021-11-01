7e28442ce726d281a6d0b67d68a816556c5613cd
elzgame <elzgamedev@gmail.com>
2021-11-01 15:51:21 +0700

    ANIMATE ZOMBIE ATTACKING

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -10,18 +10,40 @@ public class Zombie : MonoBehaviour
     public Transform player;
     private NavMeshAgent agent;
     private Animator animator;
+    private bool isAttacking;
+    private bool isWalking;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        isWalking = true;
     }
 
     void Update()
     {
-        agent.speed = zombieSpeed;
-        agent.SetDestination(player.transform.position);
-        animator.SetBool("Walk", true);
+        var distance = Vector3.Distance(player.position, transform.position);
+        Debug.Log(distance);
+        if (distance <= 2f && isAttacking == false)
+        {
+            isAttacking = true;
+            isWalking = false;
+            animator.SetBool("Walk", false);
+            animator.SetBool("Attack", true);
+            Debug.Log("Attacking");
+        } else if(distance >= 2f) {
+            isWalking = true;
+        }
+
+        if (isWalking)
+        {
+            isAttacking = false;
+            isWalking = true;
+            agent.speed = zombieSpeed;
+            agent.SetDestination(player.transform.position);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Attack", false);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
