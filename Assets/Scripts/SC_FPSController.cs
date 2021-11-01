c1cefcce3b56a146116cd3075c794e526a48dffe
elzgame <elzgamedev@gmail.com>
2021-11-02 06:28:32 +0700

    Weapon trigger and damage hold

--- Assets/Scripts/SC_FPSController.cs
+++ Assets/Scripts/SC_FPSController.cs
@@ -14,34 +14,28 @@ public class SC_FPSController : MonoBehaviour
     public float lookXLimit = 45.0f;
     private PlayerInput playerInput;
     public FixedTouchField touchField;
-
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
-
     [HideInInspector]
     public bool canMove = true;
     private bool isJumping;
+    private bool isShooting;
     public Weapon weapon;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
-        // // Lock cursor
-        // Cursor.lockState = CursorLockMode.Locked;
-        // Cursor.visible = false;
     }
 
     void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
         Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
         float vertical = input.y;
         float horizontal = input.x;
-        // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * vertical : 0;
         float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * horizontal : 0;
@@ -59,18 +53,18 @@ public class SC_FPSController : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
+        if (isShooting)
+        {
+            Shoot();
+        }
+
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
 
-        // Player and Camera rotation
         if (canMove)
         {
             rotationX += -touchField.TouchDist.y * lookSpeed;
@@ -83,29 +77,6 @@ public class SC_FPSController : MonoBehaviour
     public void Shoot()
     {
         weapon.gameObject.GetComponent<Animator>().SetBool("Shoot", true);
-        Debug.Log("Shooting!");
-        RaycastHit hit;
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
-        {
-            if (hit.transform.tag == "Zombie")
-            {
-                hit.transform.gameObject.GetComponent<Zombie>().zombieHealth -= weapon.damage;
-                moveDirection = hit.transform.gameObject.GetComponent<Rigidbody>().transform.position - transform.position;
-                moveDirection = new Vector3(moveDirection.x, moveDirection.y + 0.75f, moveDirection.z);
-                Debug.Log(moveDirection);
-                hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * 50f);
-                // hit.transform.gameObject.GetComponent<Zombie>().animator.SetBool("Knockback", true);
-                // // if (hit.transform.gameObject.GetComponent<Zombie>().animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
-                // // { 
-                // //     hit.transform.gameObject.GetComponent<Zombie>().animator.SetBool("Knockback", false);
-                // // }
-                // // else
-                // // {
-                // //     Debug.Log("playing");
-                // // }
-                Debug.Log(hit.transform.name + " : " + hit.transform.gameObject.GetComponent<Zombie>().zombieHealth);
-            }
-        }
     }
 
     public void Jump()
@@ -113,4 +84,16 @@ public class SC_FPSController : MonoBehaviour
         isJumping = true;
         Debug.Log("Jumping!");
     }
+
+    public void ShootingDown()
+    {
+        isShooting = true;
+    }
+
+    public void ShootingUp()
+    {
+        isShooting = false;
+    }
+
+
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,9 @@ public class Weapon : MonoBehaviour
 {
     public int damage;
     private Animator animator;
+    public Camera playerCamera;
+    Vector3 moveDirection = Vector3.zero;
+
 
     void Start()
     {
@@ -13,6 +16,23 @@ public class Weapon : MonoBehaviour
     public void ShootDone()
     {
         animator.SetBool("Shoot", false);
+        Debug.Log("Shooting!");
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        {
+            if (hit.transform.tag == "Zombie")
+            {
+
+                moveDirection = hit.transform.gameObject.GetComponent<Rigidbody>().transform.position - transform.position;
+                moveDirection = new Vector3(moveDirection.x, moveDirection.y + 0.75f, moveDirection.z);
+                Debug.Log(moveDirection);
+                hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * 50f);
+                hit.transform.gameObject.GetComponent<Zombie>().zombieHealth -= damage;
+                Debug.Log(hit.transform.name + " : " + hit.transform.gameObject.GetComponent<Zombie>().zombieHealth);
+            }
+        }
     }
+
+
 
 }
